b06be9e47d91825325bc86df30d2c489f8d3f943
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-10-18 17:39:06 -0300

    Criando função que seta a bandeira do cartão e capturando o ícone para ser modificado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "./css/index.css"
 
-//Fazendo a seleção do path do primeiro elemento g que está na index. Ref. da estrutura:
+//Selecionando o path do primeiro elemento g que está na index. Ref. da estrutura:
 {/* <div class="cc-bg">
     <svg>
         <g>
@@ -15,17 +15,30 @@ import "./css/index.css"
 </div> */}
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 
-//Fazendo a seleção do path do segundo elemento g que está na index.
+//Selecionando o path do segundo elemento g que está na index.
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
 
-//Modificando o atributo fill
-ccBgColor01.setAttribute("fill","green");
-ccBgColor02.setAttribute("fill","blue");
+//Selecionando a logo da bandeira
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
+//Criando função que vai pegar a bandeira do cartão
+function setCardFlag(flag){
+    
+    //Criando estrutura de dados para as cores
+    const colors = {
+        "visa":["#2D57F2","#436D99"],
+        "mastercard":["#C69347","#DF6F29"],
+        "default":["black","gray"]
+    }
+    
+    //Modificando o atributo fill, mas agora vou modificar de acordo com a bandeira do cartão que vier pelo parâmetro da função
+    // ccBgColor01.setAttribute("fill","green");
+    // colors[flag][0] é como se estivesse fazendo colors.visa[0], mas como virá pelo parâmetro da função, como um dado variável, uso []
+    ccBgColor01.setAttribute("fill",colors[flag][0]);
+    ccBgColor02.setAttribute("fill",colors[flag][1]);
+    ccLogo.setAttribute("src",`cc-${flag}.svg`);
 
-//Criando estrutura de dados para as cores
-const colors = {
-    "visa":["#2D57F2","#436D99"],
-    "mastercard":["#C69347","#DF6F29"],
-    "default":["black","gray"]
 }
+
+//Testando a função
+setCardFlag("mastercard");
